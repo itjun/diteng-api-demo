2e05efc2fed5ab213edf7b7287e81620c523e77e
IT君 <l1091462907@gmail.com>
2021-12-09 17:27:47 +0800

    增加接入指南

--- src/test/java/site/diteng/api/demo/DitengServiceTest.java
+++ src/test/java/site/diteng/api/demo/DitengServiceTest.java
@@ -9,12 +9,12 @@ public class DitengServiceTest {
 
     @Test
     public void test() {
-        String json = "{\"head\":{\"CusCode_\":\"C00398\",\"TBDate_\":\"2021-12-07\",\"ManageNo_\":\"153491985015702\"},\"body\":[[\"PartCode_\",\"Desc_\",\"Spec_\",\"Num_\",\"SpareNum_\",\"OriUP_\"],[\"0212DYRDR40350\",\"\",\"\",5,0,3],[\"0212DYRDR40350\",\"\",\"\",3,3,3],[\"11L20DLX-1\",\"\",\"\",13,0,1.5]]}";
+        String json = "{\"head\":{\"CusCode_\":\"C00397\",\"ManageNo_\":\"23422342342\",\"TBDate_\":\"2021/12/07\"},\"body\":[[\"PartCode_\",\"Desc_\",\"Spec_\",\"Num_\",\"SpareNum_\",\"OriUP_\"],[\"0212DYRDR40350\",\"\",\"\",5,0,3],[\"0212DYRDR40350\",\"\",\"\",3,3,3],[\"11L20DLX-1\",\"\",\"\",13,0,1.5]]}";
         DataSet dataSet = new DataSet().setJson(json);
         DitengService svr = new DitengService("tranod.append");
         DataRow headIn = svr.dataIn().head();
-        headIn.setValue("CusCode_", "C00398");
-        headIn.setValue("TBDate_", "2021-12-07");
+        headIn.copyValues(dataSet.head());
+//        headIn.setValue("CusCode_", "C00397");// 手动赋值到头部
 
         // 此处可以循环订单对象，然后追加生成商品列表数据集，这里直接从json数据生成dataSet了
         DataSet dataIn = svr.dataIn();
@@ -27,6 +27,7 @@ public class DitengServiceTest {
             dataIn.setValue("SpareNum_", dataSet.getString("SpareNum_"));
             dataIn.setValue("OriUP_", dataSet.getString("OriUP_"));
         }
+
         if (svr.exec())
             System.out.println(svr.dataOut());
         else
